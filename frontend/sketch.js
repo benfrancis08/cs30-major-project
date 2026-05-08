d23dae0bdead1bb9216c835ffe121f60a56f5d02
Ben Francis <ben3dprints1@gmail.com>
2026-05-08 12:06:42 -0600

    Fixed infinite looping in package.json and worked on converting object from backend back into a map in the frontend

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -8,6 +8,7 @@
 let stockPrices;
 let currentTime;
 let price = undefined;
+let stocks = new Map();
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,15 +21,18 @@ function draw() {
   background(220);
   if (millis() > currentTime + 10000) {
     currentTime = millis();
-    getPrice();
+    getStocks();
+    setupStocks();
   }
-  if (price !== undefined) {
-    text(price[2].price, width/2, height/2);
-  }
+  // console.log(price);
+  // if (price !== undefined) {
+  //   text(price[2].price, width/2, height/2);
+  // }
 }
 
-async function getPrice() {
+async function getStocks() {
   try {
+    // Fetch from localhost for testing only. SWITCH TO "https://pine64.tailb67b61.ts.net" BEFORE HANDING IN/TESTING
     stockPrices = await fetch('http://localhost:3000/prices');
     price = await stockPrices.json();
   }
@@ -36,3 +40,9 @@ async function getPrice() {
     console.log("something went wrong " + error);
   }
 } 
+
+function setupStocks() {
+  for (let [symbol, value] of Object.entries(price)) {
+    stocks.set(symbol, value);
+  }
+}
